4671929eb6c6effa206da7d058a4245e450651d3
lanavely <legojur1254@gmail.com>
2024-05-23 14:39:39 +0300

    deleted users

---
+++ Auto/Migrations/20240523113332_TimeOnly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auto.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "EndTime",
+                table: "ScheduleItems",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "ScheduleItems",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+        }
+    }
+}
