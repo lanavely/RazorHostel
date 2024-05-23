35ac1caae19d5e01851d5e6d09474e6c26c7eb6b
lanavely <legojur1254@gmail.com>
2024-05-23 14:24:50 +0300

    migration schedule

---
+++ Auto/Migrations/20240523112428_AddedSchedule.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Auto.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSchedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Groups_GroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_GroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "GroupId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PassedStatus",
+                table: "Tests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "Bookings",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScheduleItemId",
+                table: "Bookings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    ScheduleId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SchoolId = table.Column<int>(type: "integer", nullable: false),
+                    Days = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.ScheduleId);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Schools_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "Schools",
+                        principalColumn: "SchoolId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ScheduleId = table.Column<int>(type: "integer", nullable: false),
+                    StartTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScheduleItems_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "ScheduleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_ScheduleItemId",
+                table: "Bookings",
+                column: "ScheduleItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleItems_ScheduleId",
+                table: "ScheduleItems",
+                column: "ScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_SchoolId",
+                table: "Schedules",
+                column: "SchoolId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_ScheduleItems_ScheduleItemId",
+                table: "Bookings",
+                column: "ScheduleItemId",
+                principalTable: "ScheduleItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_ScheduleItems_ScheduleItemId",
+                table: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "ScheduleItems");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_ScheduleItemId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PassedStatus",
+                table: "Tests");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduleItemId",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Bookings",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Bookings",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "GroupId",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    GroupId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.GroupId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_GroupId",
+                table: "AspNetUsers",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Groups_GroupId",
+                table: "AspNetUsers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId");
+        }
+    }
+}
